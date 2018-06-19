058574e6735d1d2d0f7f1b6b2656129b1ecc029b
abanglaile <abanglaile@users.noreply.github.com>
2018-06-19 19:01:30 +0800

    合并新界面

--- src/Component/image-manager.js
+++ src/Component/image-manager.js
@@ -1,70 +1,105 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Layout, Icon, Row, Col, Menu, Select, Button, Popconfirm, Input ,Cascader, Checkbox, Tooltip} from 'antd';
-import NetUtil from '../utils/NetUtil';
-import Styles from './exerciseEdit.css';
-import Tex from './renderer.js';
-import QueueAnim from 'rc-queue-anim';
+import { Layout, Icon, Row, Col, Menu, Select, Button, Modal, Input} from 'antd';
+const { TextArea, Search } = Input;
 
 import *as action from '../Action/';
 import {connect} from 'react-redux';
 
 import FileUpload from './upload-qiniu.js';
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Sider, Content, } = Layout;
 const { SubMenu } = Menu;
 const Option = Select.Option;
 
 const server_url = "http://127.0.0.1:3000";
 
-class ExerciseTitle extends React.Component {
+class ImageManager extends React.Component {
+
+    componentDidMount(){
+      console.log(this.props.params.exercise_id);
+      this.props.getMediaList();
+    }
+
+    renderMediaList(){
+      const {media_list} = this.props;
+      return media_list.map((item) => {
+          return (
+          <Row style={{marginTop: '18px'}}>
+            <Col span={12}>
+              <div>
+                <Input value={item.url} />
+                <img src={item.url} />
+              </div>
+            </Col>
+            <Col span={12}>
+              <TextArea value={item.code} autosize={{ minRows: 8, maxRows: 12 }}/> 
+            </Col>
+          </Row>
+          )
+      })
+    }
+
+    saveModalOnOk(){
+      this.props.saveTestMedia(this.props.save_url);
+      this.props.saveModalCancel();
+    }
+
+    saveModalOnCancel(){
+      this.props.saveUrlChange("");
+      this.props.saveModalCancel();
+    }
 
     render(){
-    	var { exercise } = this.props;
-      var { title, title_img_url, title_audio_url, exercise_id } = exercise;
-  		title = title ? title :'';
+      console.log(this.props.save_url);
       return(
       		<div>
-  	    		<Row className="choice_row" gutter={16} type="flex" justify="space-between">
+  	    		<Row style={{marginTop: '18px'}}>
   	    			<Col span={12}>
-      					<Input 
-                    style={{font: "14px/1.4 proxima-nova, Helvetica Neue, Arial, Helvetica, sans-serif" }}
-                    value={title} type="textarea" onChange={(e)=>this.props.titleChange(e.target.value)} rows={12} />
-  	    			</Col>
-    					<Col span={12} style={{ background: '#fff', border: '1px solid #d9d9d9'}}>
-                <div style = {{ padding: '5px' }}>
-        					   <Tex content={title} />
+                <div>
+                  <TextArea value={this.props.test_code} autosize={{ minRows: 8, maxRows: 12 }} onChange={(e)=>this.props.codeChange(e.target.value)}/>
+                  <Button onClick={() => this.props.codeRender(this.props.test_code)}>生成图片</Button>
                 </div>
+                <Search
+                  placeholder="input search text"
+                  onSearch={(url) => this.props.searchMedia(url)}
+                  enterButton
+                  value={this.props.save_url}
+                  onChange={e => this.props.saveUrlChange(e.target.value)}
+                />
+                <div>
+                  <Button onClick={() => this.props.saveTestMedia(this.props.save_url)}>保存</Button>
+                  <Button onClick={this.props.saveModalOpen}>另存为</Button>
+                </div>  
+  	    			</Col>
+    					<Col span={12}>
+                <img src={this.props.test_url} />
     					</Col>
     				</Row>
-    				<Row gutter={16}>
-    					<Col span={12}>
-    						<FileUpload button='Title_Img' onChange={file => this.props.titleImgChange(file.url)} onRemove={() => this.props.titleImgRemove()} />
-    					</Col>
-    					<Col span={12}>
-    				    <img src={title_img_url} height="100px"/>
-                </Col>
-    				</Row>
-            <Row gutter={16}>
-              <Col span={12}>
-                <FileUpload button='Title_Audio' onChange={file => this.props.titleAudioChange(file.url)} onRemove={() => this.props.titleAudioRemove()} />
-              </Col>
-              <Col span={12}>
-                <audio src={title_audio_url} controls="controls">
-                    Your browser does not support the audio element.
-                </audio>
-              </Col>
-            </Row>
+    				
+            {this.renderMediaList()}
+            <Modal
+              title="Save media as this url?"
+              visible={this.props.modal_open}
+              onOk={() => this.saveModalOnOk()}
+              onCancel={() => this.saveModalOnCancel()}
+            >
+              <Input value={this.props.save_url} onChange={e => this.props.saveUrlChange(e.target.value)} />
+            </Modal>
       		</div>
       	);
     }
 }
 
 export default connect(state => {
-  var newState = state.exerciseData.toJS();
+  var newState = state.imageData.toJS();
   return {
-    exercise : newState.exercise,
+    test_url : newState.test_url,
+    test_code: newState.test_code,
+    save_url: newState.save_url,
+    media_list: newState.media_list,
+    modal_open: newState.modal_open,
     isLoading: newState.isLoading,
   }
-}, action)(ExerciseTitle);
+}, action)(ImageManager);
 
 
